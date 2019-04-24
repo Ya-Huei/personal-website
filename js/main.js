7716c3c2cc093c2e6e474f7697e2209383ea1de1
yahuei <ipad010682@gmail.com>
2019-04-24 16:16:06 +0800

    modify to navbar

--- js/main.js
+++ js/main.js
@@ -3,18 +3,20 @@
  	easing: 'slide'
  });
 
+
 (function($) {
 
 	"use strict";
-
+	$("#nav").load("nav.html");
+	
 	$(window).stellar({
-    responsive: true,
-    parallaxBackgrounds: true,
-    parallaxElements: true,
-    horizontalScrolling: false,
-    hideDistantElements: false,
-    scrollProperty: 'scroll'
-  });
+	    responsive: true,
+	    parallaxBackgrounds: true,
+	    parallaxElements: true,
+	    horizontalScrolling: false,
+	    hideDistantElements: false,
+	    scrollProperty: 'scroll'
+	});
 
 
 	var fullHeight = function() {
